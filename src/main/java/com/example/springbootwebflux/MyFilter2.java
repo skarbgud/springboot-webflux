c58aad76544803459ecec0cbfd3e6bbe51408e55
GyuHyeong Nam <sjarbgud@gmail.com>
2022-02-06 07:33:41 +0900

    raw 코드로 작성

---
+++ src/main/java/com/example/springbootwebflux/MyFilter2.java
@@ -0,0 +1,23 @@
+package com.example.springbootwebflux;
+
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyFilter2 implements Filter {
+
+    private EventNotify eventNotify;
+
+    public MyFilter2(EventNotify eventNotify) {
+        this.eventNotify = eventNotify;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("필터2 실행됨");
+        // 데이터 하나 발생시킴
+        eventNotify.add("새로운 데이터");
+    }
+}
